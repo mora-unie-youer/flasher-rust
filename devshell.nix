d24a0303b811d4073b43bb4ec0339878b8a21d51
Mora Unie Youer <mora_unie_youer@riseup.net>
2023-06-08 18:56:12 +0000

    Initial commit

---
+++ devshell.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+
+pkgs.devShell.mkShell {
+  name = "flasher";
+  packages = with pkgs; [
+    # Toolchain required for C + Rust binaries building
+    binutils
+    gcc
+
+    # Nightly Rust toolchain
+    bacon
+    flamegraph
+    hyperfine
+    (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+      # Extensions which ease your development process
+      extensions = [ "rust-analyzer" "rust-src" ];
+    }))
+  ];
+}
